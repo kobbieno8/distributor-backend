b61fff5cfea1b33284c19ddb5e02e37ac613b97b
Kdus17 <kidus2460@gmail.com>
2025-08-06 15:11:50 +0300

    serv

--- Server/Server.js
+++ Server/Server.js
@@ -4,16 +4,22 @@ const routes = require("./routes/routes");
 const port = process.env.PORT;
 const mongoose = require("mongoose");
 
+const cors = require("cors");
 const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use("/local", routes);
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://distributer-website.vercel.app",
-];
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://distributer-website.vercel.app"],
+  methods: ["GET", "PUT", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  maxAge: 3600,
+};
+
+app.use(cors(corsOptions));
 
 const dbConnect = async () => {
   try {

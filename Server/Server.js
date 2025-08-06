85ca5a412bece4b6519094c35d2d1ef4dd3df8ad
Abel Gulbet <gulbetabel@gmail.com>
2025-08-06 22:06:02 +0300

    might just workout

--- Server/Server.js
+++ Server/Server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const routes = require("./routes/routes");
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 
 const cors = require("cors");
